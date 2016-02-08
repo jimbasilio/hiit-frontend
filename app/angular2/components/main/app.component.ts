f0714639e72e744c95d5115606510d88523a8e91
Jim Basilio <jbasilio@seic.com>
2016-02-08 14:53:48 -0500

    .

--- app/angular2/components/main/app.component.ts
+++ app/angular2/components/main/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'hiit',
-    template: '<h1> My First Angular 2 App - hiit</h1>'
+    templateUrl: 'app.main.html'
 })
 export class AppComponent { }
--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-if [ ! -d logs ] 
-then
-  mkdir logs
-fi
-
 sudo docker run -d -v $(pwd)/../dist:/usr/local/apache2/htdocs --name=hiit-frontend -p 8080:80 -t hiit-frontend
 
