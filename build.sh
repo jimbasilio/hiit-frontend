510ccb2286117858c4c2100844a8f58d7199fb60
Jim Basilio <jim.basilio@gmail.com>
2016-03-20 16:41:51 -0400

    switched to SASS, adjusted build chain for SASS

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ ! -d node_modules ]; then
   npm install
 fi
 
-npm run build
+npm --silent run build
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 // started from here => http://blog.scottlogic.com/2015/12/24/creating-an-angular-2-build.html
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const del = require('del');
 const typescript = require('gulp-typescript');
 const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
 const tslint = require('gulp-tslint');
 const tsconfig = require('tsconfig-glob');
+const autoprefixer = require('gulp-autoprefixer');
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
@@ -26,7 +28,7 @@ gulp.task('copy:libs', ['clean'], function() {
       'node_modules/angular2/bundles/router.js',
       'node_modules/es6-shim/es6-shim.js',
       'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/bootstrap/dist/css/bootstrap.css',
+      'node_modules/bootstrap-sass/dist/css/bootstrap.css',
       'node_modules/bootstrap/dist/js/bootstrap.js',
       'node_modules/font-awesome/css/font-awesome.css',
       'node_modules/satellizer/satellizer.js',
@@ -39,9 +41,15 @@ gulp.task('copy:libs', ['clean'], function() {
     .pipe(gulp.dest('dist/lib'))
 });
 
+gulp.task('copy:ngrx', ['clean'], function() {
+    return gulp
+        .src(['node_modules/@ngrx/**/dist/*'])
+        .pipe(gulp.dest('dist/lib/@ngrx'));
+})
+
 // font-awesome fonts
 gulp.task('icons', ['clean'], function() {
-    return gulp.src('node_modules/font-awesome/fonts/**.*')
+    return gulp.src(['node_modules/font-awesome/fonts/**.*', 'node_modules/bootstrap-sass/assets/fonts/bootstrap/**/*'])
         .pipe(gulp.dest('dist/fonts'));
 });
 
@@ -49,7 +57,20 @@ gulp.task('icons', ['clean'], function() {
 gulp.task('tslint', function() {
   return gulp.src('app/**/*.ts')
     .pipe(tslint())
-    .pipe(tslint.report('verbose'));
+    .pipe(tslint.report('msbuild'));
+});
+
+// linting
+gulp.task('tslintprod', function() {
+  return gulp.src('app/**/*.ts')
+    .pipe(tslint({
+        configuration: {
+            rules: {
+                "no-debugger": "true"
+            }
+        }
+    }))
+    .pipe(tslint.report('msbuild'));
 });
 
 
@@ -59,8 +80,27 @@ gulp.task('compile', ['clean'], function () {
     .src(tscConfig.files)
     .pipe(sourcemaps.init())
     .pipe(typescript(tscConfig.compilerOptions))
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write('.', { includeContent: true, sourceRoot: '/src'}))
     .pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('compile:sass', ['clean'], function() {
+    return gulp
+        .src(['src/app/stylesheets/**/*.css', 'src/app/stylesheets/**/*.scss'])
+        .pipe(sass({
+            errLogToConsole: true,
+            outputStyle: 'expanded',
+            includePaths: [
+                'node_modules/bootstrap-sass/assets/stylesheets', 'node_modules/font-awesome/scss'
+            ]
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: [
+                'last 2 versions'
+            ]
+        }))
+        .pipe(gulp.dest('dist/css/'));
 });
 
 // update the tsconfig files based on the glob pattern
@@ -71,6 +111,6 @@ gulp.task('tsconfig-glob', function () {
   });
 });
 
-gulp.task('build', ['tslint', 'tsconfig-glob', 'compile', 'copy:libs', 'icons', 'copy:assets']);
+gulp.task('build', ['tslint', 'tsconfig-glob', 'compile', 'compile:sass', 'copy:libs', 'icons', 'copy:assets']);
 
 gulp.task('default', ['build']);
